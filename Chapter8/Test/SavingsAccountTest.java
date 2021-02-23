7edbed4f6d85201f2ffe114a3b2d1863364294e9
Olugbenga Alao <gasttronomy@gmail.com>
2021-02-23 05:07:26 +0100

    Few exercises

---
+++ Chapter8/Test/SavingsAccountTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SavingsAccountTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    SavingsAccount saver1 = new SavingsAccount();
+}
---
+++ Chapter_14/src/PigLatin.java
@@ -0,0 +1,7 @@
+/*14.6 (Project: Limericks) A limerick is a humorous five-line verse in which the first and second lines
+rhyme with the fifth, and the third line rhymes with the fourth. Using techniques similar to those developed
+in Exercise 14.5, write a Java application that produces random limericks. Polishing this application
+to produce good limericks is a challenging problem, but the result will be worth the effort!*/
+
+public class Limerick {
+}
